58694a731de19d5a4ac5c8f3b7bbc12b7cbfa28f
hoangbao27092004 <hoangbao27092004@gmail.com>
2024-10-01 08:59:05 +0700

    Init for Ex3

--- Exercise/Core/Src/main.c
+++ Exercise/Core/Src/main.c
@@ -24,6 +24,7 @@
 /* USER CODE BEGIN Includes */
 #include "software_timer.h"
 #include "Component.h"
+#include "update7SEG.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -57,7 +58,8 @@ static void MX_TIM2_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+const int MAX_LED = 4;
+int index_led = 0;
 /* USER CODE END 0 */
 
 /**
@@ -96,55 +98,34 @@ int main(void)
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   setTimer1(50);
-  int counter = 1, bit = 0;
-  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 0);
-  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
-  HAL_GPIO_WritePin(EN1_GPIO_Port, EN2_Pin, 1);
-  HAL_GPIO_WritePin(EN1_GPIO_Port, EN3_Pin, 1);
-  display7SEG(counter++);
+  int counter = 0, bit = 0;
   while (1)
   {
 	  if(timer1_flag == 1){
 		  setTimer1(50);
-		  if(counter == 1 || counter == 3){
+		  if(counter == 0){
 			  if(bit == 0){
-				  HAL_GPIO_WritePin(DOT_GPIO_Port, DOT_Pin, 1);
+				  HAL_GPIO_WritePin(DOT_GPIO_Port, DOT_Pin, 0);
 				  bit = 1;
 			  }
 			  else{
-				  HAL_GPIO_WritePin(DOT_GPIO_Port, DOT_Pin, 0);
+				  HAL_GPIO_WritePin(DOT_GPIO_Port, DOT_Pin, 1);
 				  bit = 0;
 			  }
 		  }
-		  if(counter == 1){
-			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 0);
-			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
-			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN2_Pin, 1);
-			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN3_Pin, 1);
-			  display7SEG(counter++);
-		  }else if(counter == 2){
-			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
-			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 0);
-			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN2_Pin, 1);
-			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN3_Pin, 1);
-			  display7SEG(counter++);
-		  }else if(counter == 3){
-			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
-			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
-			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN2_Pin, 0);
-			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN3_Pin, 1);
-			  display7SEG(counter++);
-		  }else{
-			  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 1);
-			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
-			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN2_Pin, 1);
-			  HAL_GPIO_WritePin(EN1_GPIO_Port, EN3_Pin, 0);
-			  display7SEG(0);
-			  counter = 1;
+		  if(counter++ == 1){
+			  counter = 0;
+		  }
+		  update7SEG(index_led++);
+		  if(index_led == 4){
+			  index_led = 0;
 		  }
 	  }
-  /* USER CODE END 3 */
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
   }
+  /* USER CODE END 3 */
 }
 
 /**
